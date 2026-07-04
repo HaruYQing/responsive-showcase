9618fa3e421aa8d1a495a6ce436ca357e72abd65
HaruYQing <haru61410@gmail.com>
2026-07-04 16:05:41 +0800

    refactor: better article-grid style

--- src/app/shared/article-grid/article-grid.ts
+++ src/app/shared/article-grid/article-grid.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { BreakpointService } from './../../core/services/breakpoint.service';
+import { Component, inject, input } from '@angular/core';
 import { ArticleCard, Article } from '../article-card/article-card';
 
 @Component({
@@ -6,8 +7,15 @@ import { ArticleCard, Article } from '../article-card/article-card';
   imports: [ArticleCard],
   templateUrl: './article-grid.html',
   styleUrl: './article-grid.scss',
+  host: {
+    '[class.--side-p-80]': 'breakpointService.isDesktop()',
+    '[class.--side-p-40]': 'breakpointService.isTablet()',
+    '[class.--side-p-16]': 'breakpointService.isMobile()',
+  },
 })
 export class ArticleGrid {
+  breakpointService = inject(BreakpointService);
+
   title = input.required<string>();
   articles = input.required<Article[]>();
 }
