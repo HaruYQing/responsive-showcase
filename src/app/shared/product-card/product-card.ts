42cea010c0baadb90ccfc13d9212fa9853bb82be
HaruYQing <haru61410@gmail.com>
2026-07-04 11:53:49 +0800

    feat: product card component

---
+++ src/app/shared/product-card/product-card.ts
@@ -0,0 +1,21 @@
+import { Component, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+export interface Product {
+  title: string;
+  description: string;
+  imageUrl: string;
+  tag?: string;
+}
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.scss',
+})
+export class ProductCard {
+  product = input.required<Product>();
+}
